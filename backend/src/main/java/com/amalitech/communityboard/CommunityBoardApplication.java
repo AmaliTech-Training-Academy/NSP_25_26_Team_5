4d425ae70a3291bb52b9195fc17a23fbfc3ada29
kakwezi peace <peacekakwezi222@gmail.com>
2026-03-09 22:04:03 +0200

    ft: main class of the application

--- backend/src/main/java/com/amalitech/communityboard/CommunityBoardApplication.java
+++ backend/src/main/java/com/amalitech/communityboard/CommunityBoardApplication.java
@@ -2,8 +2,10 @@ package com.amalitech.communityboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CommunityBoardApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommunityBoardApplication.class, args);
