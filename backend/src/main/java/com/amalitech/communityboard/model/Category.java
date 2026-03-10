4424682cc55ab4bdac8f536fa743c5dd2e519c49
kakwezi peace <peacekakwezi222@gmail.com>
2026-03-10 08:50:57 +0200

    ft: Add category

--- backend/src/main/java/com/amalitech/communityboard/model/Category.java
+++ backend/src/main/java/com/amalitech/communityboard/model/Category.java
@@ -10,7 +10,6 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(unique = true, nullable = false)
     private String name;
 
