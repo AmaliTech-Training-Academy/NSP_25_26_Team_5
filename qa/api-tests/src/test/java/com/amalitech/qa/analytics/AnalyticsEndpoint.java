bb1789e49f932fed90b72dabe266e712a96a8ba0
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-03-11 09:43:30 +0200

    feat(analytics): add AnalyticsEndpoint

---
+++ qa/api-tests/src/test/java/com/amalitech/qa/analytics/AnalyticsEndpoint.java
@@ -0,0 +1,10 @@
+package com.amalitech.qa.analytics;
+
+import com.amalitech.qa.config.Constants;
+
+public final class AnalyticsEndpoint {
+
+    private AnalyticsEndpoint() {}
+
+    public static final String ANALYTICS = Constants.ANALYTICS;
+}
