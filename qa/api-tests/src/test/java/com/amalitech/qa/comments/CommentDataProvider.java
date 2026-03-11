34ce8a01c73386c799e2ff92365367023da600a4
hasby-shanessa <umutoniwabohasby@gmail.com>
2026-03-11 09:42:08 +0200

    feat(comments): add CommentEndpoint, CommentPayload, CommentDataProvider, CommentTest

--- qa/api-tests/src/test/java/com/amalitech/qa/comments/CommentDataProvider.java
+++ qa/api-tests/src/test/java/com/amalitech/qa/comments/CommentDataProvider.java
@@ -1,4 +1,21 @@
 package com.amalitech.qa.comments;
 
-public class CommentDataProvider {
+import com.amalitech.qa.base.BaseTest;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public final class CommentDataProvider extends BaseTest {
+
+    private CommentDataProvider() {}
+
+    public static Stream<Arguments> commentBoundaries() {
+        return loadTestData("testdata/comments/comment_boundaries.json")
+                .stream()
+                .map(row -> Arguments.of(
+                        row.get("label"),
+                        ((Number) row.get("length")).intValue(),
+                        ((Number) row.get("expectedStatus")).intValue()
+                ));
+    }
 }
--- qa/api-tests/src/test/java/com/amalitech/qa/comments/CommentEndpoint.java
+++ qa/api-tests/src/test/java/com/amalitech/qa/comments/CommentEndpoint.java
@@ -1,4 +1,11 @@
 package com.amalitech.qa.comments;
 
-public class CommentEndpoint {
+import com.amalitech.qa.config.Constants;
+
+public final class CommentEndpoint {
+
+    private CommentEndpoint() {}
+
+    public static final String COMMENTS      = Constants.COMMENTS;
+    public static final String COMMENT_BY_ID = Constants.COMMENT_BY_ID;
 }
