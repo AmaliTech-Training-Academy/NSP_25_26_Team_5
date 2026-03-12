307dd4093de5eeeaddf874053384167fc56e67fc
rodriguecyber <rodrirwigara@gmail.com>
2026-03-12 16:12:53 +0200

    run tf  terraform fmt -recursive

--- devops/infra/terraform/modules/ec2_app/main.tf
+++ devops/infra/terraform/modules/ec2_app/main.tf
@@ -22,8 +22,8 @@ resource "aws_iam_role_policy_attachment" "ecr" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_iam_role_policy" "sns" {
-  name   = "${var.project_name}-ec2-sns"
-  role   = aws_iam_role.app.id
+  name = "${var.project_name}-ec2-sns"
+  role = aws_iam_role.app.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
